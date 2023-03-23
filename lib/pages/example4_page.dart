8e0503da89570e92f2e999498d5cef6bee96fd87
Sonia Justiniano <sjustinianoayte@gmail.com>
2023-03-22 22:12:27 -0500

    Mods

--- lib/pages/example4_page.dart
+++ lib/pages/example4_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Example4Page extends StatelessWidget {
   @override
@@ -18,10 +19,93 @@ class Example4Page extends StatelessWidget {
       ),
       child: Scaffold(
         backgroundColor: Colors.transparent,
-        body: Column(
-          children: const [
-            TextField(),
-          ],
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(30.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Center(
+                  child: Text(
+                    "Burger \nQueen",
+                    style: GoogleFonts.fredokaOne(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w600,
+                      fontSize: 30.0,
+                      height: 1.0,
+                    ),
+                  ),
+                ),
+                const Expanded(
+                  child: SizedBox(),
+                ),
+                Text(
+                  "Burger Queen",
+                  style: GoogleFonts.fredokaOne(
+                    color: Colors.white,
+                    fontSize: 36.0,
+                    fontWeight: FontWeight.w800,
+                    decoration: TextDecoration.underline,
+                    decorationColor: const Color(0xffFF8D07).withOpacity(0.5),
+                    decorationThickness: 2.5,
+                  ),
+                ),
+                Text(
+                  "Tasty Food *",
+                  style: GoogleFonts.fredokaOne(
+                    color: Colors.white,
+                    fontSize: 36.0,
+                    fontWeight: FontWeight.w800,
+                    decoration: TextDecoration.underline,
+                    decorationColor: const Color(0xffFF8D07).withOpacity(0.5),
+                    decorationThickness: 2.5,
+                  ),
+                ),
+                const SizedBox(
+                  height: 16.0,
+                ),
+                SizedBox(
+                  width: double.infinity,
+                  height: 54.0,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xffFF8D07),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(14.0),
+                      ),
+                    ),
+                    child: const Text(
+                      "Sign in",
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 12.0,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Text(
+                      "Don´t have an account?  ",
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                    Text(
+                      "Sign up",
+                      style: TextStyle(
+                        color: Colors.white,
+                        decoration: TextDecoration.underline,
+                        decorationColor: Colors.white,
+                        decorationThickness: 2.5,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
