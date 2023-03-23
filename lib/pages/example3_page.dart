80e3c861c340792685526dc0f29ef670834a583a
Sonia Justiniano <sjustinianoayte@gmail.com>
2023-03-22 20:20:31 -0500

    Mods

--- lib/pages/example3_page.dart
+++ lib/pages/example3_page.dart
@@ -35,107 +35,139 @@ class ExamplePage3 extends StatelessWidget {
 
           //Formulario
           SafeArea(
-            child: Padding(
-              padding: const EdgeInsets.all(26.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "reisup",
-                    style: GoogleFonts.montserrat(
-                      color: const Color(0xff3EA2A8),
-                      fontSize: 30.0,
-                      fontWeight: FontWeight.w700,
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(26.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      height: height * 0.14,
                     ),
-                  ),
-                  const Text(
-                    "You have goals",
-                    style: TextStyle(
-                      color: Color(0xff2E303E),
-                      fontSize: 24.0,
-                      fontWeight: FontWeight.w400,
-                      letterSpacing: -0.9,
-                    ),
-                  ),
-                  const Text(
-                    "Invest to archive them",
-                    style: TextStyle(
-                      color: Color(0xff2E303E),
-                      fontSize: 24.0,
-                      fontWeight: FontWeight.w400,
-                      letterSpacing: -0.9,
-                    ),
-                  ),
-                  TextField(
-                    decoration: InputDecoration(
-                      hintText: "Email*",
-                      hintStyle: GoogleFonts.montserrat(color: Colors.black38),
-                      enabledBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(
-                          color: const Color(0xff3EA2A8).withOpacity(0.50),
-                          width: 2.5,
-                        ),
-                      ),
-                      focusedBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(
-                          color: const Color(0xff3EA2A8).withOpacity(0.50),
-                          width: 2.5,
-                        ),
+                    Text(
+                      "reisup",
+                      style: GoogleFonts.montserrat(
+                        color: const Color(0xff3EA2A8),
+                        fontSize: 30.0,
+                        fontWeight: FontWeight.w700,
                       ),
                     ),
-                  ),
-                  TextField(
-                    decoration: InputDecoration(
-                      hintText: "Password*",
-                      hintStyle: GoogleFonts.montserrat(color: Colors.black38),
-                      enabledBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(
-                          color: const Color(0xff3EA2A8).withOpacity(0.50),
-                          width: 2.5,
-                        ),
+                    const Text(
+                      "You have goals",
+                      style: TextStyle(
+                        color: Color(0xff2E303E),
+                        fontSize: 24.0,
+                        fontWeight: FontWeight.w400,
+                        letterSpacing: -0.9,
                       ),
-                      focusedBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(
-                          color: const Color(0xff3EA2A8).withOpacity(0.50),
-                          width: 2.5,
+                    ),
+                    const Text(
+                      "Invest to archive them",
+                      style: TextStyle(
+                        color: Color(0xff2E303E),
+                        fontSize: 24.0,
+                        fontWeight: FontWeight.w400,
+                        letterSpacing: -0.9,
+                      ),
+                    ),
+                    TextField(
+                      decoration: InputDecoration(
+                        hintText: "Email*",
+                        hintStyle:
+                            GoogleFonts.montserrat(color: Colors.black38),
+                        enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(
+                            color: const Color(0xff3EA2A8).withOpacity(0.50),
+                            width: 2.5,
+                          ),
+                        ),
+                        focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(
+                            color: const Color(0xff3EA2A8).withOpacity(0.50),
+                            width: 2.5,
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                  SizedBox(
-                    width: double.infinity,
-                    height: 56,
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: const Color(0xff2E303E),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(14.0),
+                    TextField(
+                      decoration: InputDecoration(
+                        hintText: "Password*",
+                        hintStyle:
+                            GoogleFonts.montserrat(color: Colors.black38),
+                        enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(
+                            color: const Color(0xff3EA2A8).withOpacity(0.50),
+                            width: 2.5,
+                          ),
+                        ),
+                        focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(
+                            color: const Color(0xff3EA2A8).withOpacity(0.50),
+                            width: 2.5,
+                          ),
                         ),
                       ),
-                      child: const Text("Log in"),
                     ),
-                  ),
-                  const SizedBox(
-                    height: 14.0,
-                  ),
-                  SizedBox(
-                    width: double.infinity,
-                    height: 56,
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: const Color(0xff2E303E),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(14.0),
+                    SizedBox(
+                      height: height * 0.06,
+                    ),
+                    SizedBox(
+                      width: double.infinity,
+                      height: 56,
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color(0xff2E303E),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(14.0),
+                          ),
+                        ),
+                        child: const Text("Log in"),
+                      ),
+                    ),
+                    SizedBox(
+                      height: height * 0.04,
+                    ),
+                    SizedBox(
+                      width: double.infinity,
+                      height: 56,
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color(0xff2E303E),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(14.0),
+                          ),
+                        ),
+                        child: const Text(
+                          "Sign app",
                         ),
                       ),
-                      child: const Text(
-                        "Sign app",
+                    ),
+                    SizedBox(
+                      height: height * 0.04,
+                    ),
+                    Text(
+                      "Your username or password?",
+                      style: GoogleFonts.montserrat(
+                        fontSize: 16.0,
+                        color: const Color(0xff2E303E),
+                        fontWeight: FontWeight.w600,
                       ),
                     ),
-                  ),
-                ],
+                    SizedBox(
+                      height: height * 0.03,
+                    ),
+                    Text(
+                      "By proceding you also agree to the therms of Service nd Privacy Pollcy",
+                      style: GoogleFonts.montserrat(
+                        fontSize: 14.0,
+                        color: const Color(0xff2E303E).withOpacity(0.6),
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
